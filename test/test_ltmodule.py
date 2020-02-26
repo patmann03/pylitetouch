7eb10b3d7b6083004318222e049e237aa1f41f7c
patmann03 <Patrick.Carr@fresenius-kabi.com>
2020-02-26 14:45:52 -0600

    Update test script with updated function names

--- test/test_ltmodule.py
+++ test/test_ltmodule.py
@@ -5,25 +5,28 @@ import time
 host = '192.168.1.65'
 port = 10001
 
+load = 105
+lvl = 90
+kp = 22
+but = 1
+
 def callback(msg, args):
     """Show the message are arguments."""
     print(msg, args)
 
 
 
-LT = pylitetouch.LiteTouch(host,port,callback)
+LT = pylitetouch.pylitetouch.LiteTouch(host,port,callback)
 
-load = 105
 
-lvl = 90
 
 #LT.set_loadlevel(load,lvl)
 
 #LT.set_loadon(load)
-LT.get_LEDStates(22,1)
+LT.get_led_states(kp,but)
 #LT.set_loadoff(load)
 
-#LT.toggleSwitch(16,2)
+#LT.toggle_switch(kp,but)
 
 print('Waiting for Messages')
 time.sleep(10.)
